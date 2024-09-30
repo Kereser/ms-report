fd74cd19c016e48e095d5e09f91a97630ef4a949
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-29 22:06:21 -0500

    Add logic to sale flow

--- src/main/java/com/emazon/ms_report/infra/input/rest/ReportController.java
+++ src/main/java/com/emazon/ms_report/infra/input/rest/ReportController.java
@@ -1,19 +1,27 @@
 package com.emazon.ms_report.infra.input.rest;
 
-import com.emazon.ms_report.ConstUtils;
+import com.emazon.ms_report.ConsUtils;
 import com.emazon.ms_report.application.dto.ReportReqDTO;
+import com.emazon.ms_report.application.handler.IReportHandler;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(ConstUtils.BASIC_URL)
+@RequestMapping(ConsUtils.BASIC_URL)
+@RequiredArgsConstructor
 public class ReportController {
 
+    private final IReportHandler reportHandler;
+
     @PostMapping
-    public ResponseEntity<Void> createReport(ReportReqDTO reportReqDTO) {
+    public ResponseEntity<Void> createReport(@RequestBody @Valid ReportReqDTO reportReqDTO) {
+        reportHandler.generateReport(reportReqDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
